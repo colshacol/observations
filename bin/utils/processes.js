acde7b7f77368eb126027a0e16e2f948e77f1f32
Colton Colcleasure <colshacol@gmail.com>
2017-05-09 17:15:41 +0000

    ...Always forget to .gitignore...

--- bin/utils/processes.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function killProcesses(processes) {
-    console.log(`[Observations:] Killing ${processes.length} processes.`);
-    !!processes.length && processes.map((process) => {
-        process.kill('SIGHUP');
-    });
-}
-exports.killProcesses = killProcesses;
